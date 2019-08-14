77afb252d654e0ad0c7532c67d72897f497cab2e
张斌 <q243132465@163.com>
2019-08-14 17:47:32 +0800

    second-commit

---
+++ sentinel-app/src/test/java/com/gupaovip/sentinel/sentinelapp/SentinelAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gupaovip.sentinel.sentinelapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SentinelAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
